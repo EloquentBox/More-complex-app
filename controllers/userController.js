34398092cc5b94c804f5bd474224154fc1ecaba6
Karol <drogakrzyzowa@gmail.com>
2021-01-02 21:11:29 +0000

    Simple controller inside router - all notes are in

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+exports.login = () => {
+
+}
+
+exports.logout = () => {
+
+}
+
+exports.register = (req, res) => {
+    res.send('thanks for trying ')
+}
+
+exports.home = (req, res) => {
+    res.render('home-guest');
+}
