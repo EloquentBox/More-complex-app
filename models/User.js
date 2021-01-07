f4c83c0eb39b61636d38a9b422021ad06c353ba2
Karol <drogakrzyzowa@gmail.com>
2021-01-07 20:47:04 +0000

    Easy way to hash passwords added to our database

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const usersCollection = require('../db').collection('users')                    // database object
 const validator = require('validator');
 
@@ -25,7 +26,7 @@ User.prototype.validate = function () {
     if(!validator.isEmail(this.data.email)) {this.errors.push('You must provide an email')}
     if(this.data.password === '') {this.errors.push('You must provide a password')}
     if(this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push('Password must be at least 12 characters')}
-    if(this.data.password.length > 100) {this.errors.push('Password cannot exceed 100 characters')}
+    if(this.data.password.length > 50) {this.errors.push('Password cannot exceed 50 characters')}
     if(this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push('Username must be at least 12 characters')}
     if(this.data.username.length > 100) {this.errors.push('Username cannot exceed 100 characters')}
 }
@@ -34,7 +35,7 @@ User.prototype.login = function() {
     return new Promise((resolve, reject) => {
             this.cleanUp()
             usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
-                if(attemptedUser && attemptedUser.password === this.data.password) {
+                if(attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
                     resolve('Congrats')
                 } else {
                     reject('Invalid username / pass')
@@ -52,6 +53,9 @@ User.prototype.register = function () {
     // step #2 Only if there are no validation errors
     // then save user data into a database
     if(!this.errors.length) {
+        // hash user password
+        let salt = bcrypt.genSaltSync(10)
+        this.data.password = bcrypt.hashSync(this.data.password, salt)
         usersCollection.insertOne(this.data)                            // CRUD - Create new document in DB with data without errors
     }
 };
